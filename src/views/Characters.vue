80967050448e894d1eb25f41223b339f6397949b
Sebastian peralta <juan.peralta@unillanos.edu.co>
2020-11-06 01:06:20 -0500

    fix: size of card horizontal in characters

--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -64,7 +64,7 @@ export default {
 
         .list {
             display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+            grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
             gap: 15px;
             list-style: none;
         }
